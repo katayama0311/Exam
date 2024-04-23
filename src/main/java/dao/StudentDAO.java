897c870ee8f52e286958686e555a9d3e200ade07
honda <honda@LAPTOP-404BHD63>
2024-04-23 10:40:55 +0900

    a

---
+++ src/main/java/dao/StudentDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.Student;
+
+public class StudentDAO extends DAO{
+	public Student get(String no) throws Exception {
+		
+		Connection con = getConnection();
+		
+		PreparedStatement st = con.prepareStatement("select * from student")
+				st.setString(1,no);
+		ResultSet rs = st.executeQuery();
+		
+		
+	        Student student =new Student();
+	        student.setEntYear(rs.getInt("entYear"));
+	        student.setNo(no);
+	        student.setName(rs.getString("name"));
+	        student.setClassNum(rs.getString("classNum"));
+	        student.setAttend(rs.getBoolean("isAttend"));
+	        
+	        st.close();
+	        con.close();
+	        
+	        return student;
+
+	    
+		
+	}
