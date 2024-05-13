e5f4580f91252f8ea0b961ef2a7ad6d6a29598ed
t_katayama <t_katayama@LAPTOP-5FARS7VE>
2024-05-13 12:00:37 +0900

    むりぽ

--- src/main/java/testlist/TestListAction.java
+++ src/main/java/testlist/TestListAction.java
@@ -1,6 +1,12 @@
 package testlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Student;
 import bean.Test;
+import dao.StudentDAO;
+import dao.SubjectDAO;
 import dao.TestDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,29 +21,62 @@ public class TestListAction extends Action {
 		
 		HttpSession session=request.getSession();
 		
-		String f1=request.getParameter("ent_year");
-		String f2=request.getParameter("class_num");
-		String f3=request.getParameter("subject_cd");
-		String f4=request.getParameter("no");
+		int ent_year=Integer.parseInt(request.getParameter("f1"));
+		String class_num=request.getParameter("f2");
+		String subject_cd=request.getParameter("f3");
+		int no=Integer.parseInt(request.getParameter("f4"));
 		
-		TestDAO dao=new TestDAO();
-		Test test=dao.search(f1, f2, f3, f4);
+		if(ent_year == 0 || class_num == null || subject_cd == null || no == 0) {
+			request.setAttribute("none_error", "入学年度とクラスと科目と回数を選択してください");
+			return "エラー画面のjsp";
+		}
 		
+		String scd = request.getParameter("scd");
 		
-	}
-	
-	
-	public void setTestListSubject(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-				
-	}
-	
-	
-	public void setTestListStudent(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-					
+		TestDAO testdao=new TestDAO();
+		List<Test> t=testdao.search(ent_year, class_num, subject_cd, no);
+		
+		StudentDAO studentdao=new StudentDAO();
+		List<Student> ent=studentdao.searchent();
+		
+		List<Test> s = studentdao.searchtest(ent_year, class_num, subject_cd,no);
+		
+		if (t.size() != s.size()) {
+			List<Test> tas = new ArrayList<>();
+			if (t.size() != 0) {
+				iflabel:for(Test i:s) {
+					for(Test j:t) {
+						if(i.getStudent_no() == j.getStudent_no()) {
+							tas.add(j);
+							continue iflabel;
+						}
+					}
+					tas.add(i);
+				}
+			} else {
+				for(Test i:s) {
+					tas.add(i);
+				}
+			}
+			
+			Class_numDAO classdao=new Class_numDAO();
+			List<Class_num> classnumber = classdao.search(scd);
+			
+			SubjectDAO subjectdao=new SubjectDAO();
+			List<Subject> subject=subjectdao.search(scd);
+			
+			session.setAttribute("classnumber", classnumber);
+			session.setAttribute("ent", ent);
+			session.setAttribute("subject", subject);
+			session.setAttribute("no", no);
+			session.setAttribute("subject_cd", subject_cd);
+			session.setAttribute("ent_year", ent_year);
+			session.setAttribute("class_num", class_num);
+			session.setAttribute("test", tas);
+			
+			return "grades-mgt.jsp";
+		}
 	}
 }
+			
 
